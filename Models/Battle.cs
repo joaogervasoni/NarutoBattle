e89f49f5c662fbbdadcb854bbb895ce12cccf1d7
joaoggs <joaogabrielggs@gmail.com>
2018-06-07 21:58:01 -0300

    Initial base

---
+++ Models/Battle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class Battle
+    {
+
+        //Red Team = agress
+        //Blue team = IA
+    }
+}
