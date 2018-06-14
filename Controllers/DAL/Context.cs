e1ea95d8c7a44eb70606ff149c92cca32747c59a
joaoggs <joaogabrielggs@gmail.com>
2018-06-14 20:53:44 -0300

    Context created and SQLite nuget added

---
+++ Controllers/DAL/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Controllers.DAL
+{
+    class Context
+    {
+        SQLiteConnection conn;
+        public Context()
+        {
+            conn = new SQLiteConnection("Data Source=;Version=3;");
+        }
+    }
+}
