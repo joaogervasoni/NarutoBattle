2f5dd5a74f88105b2194572407b7adec91c9f24d
joaoggs <joaogabrielggs@gmail.com>
2018-06-19 18:56:59 -0300

    Models moved to Controller

--- Controllers/BattleController.cs
+++ Controllers/BattleController.cs
@@ -11,8 +11,16 @@ using System.Threading.Tasks;
 namespace Controllers
 {
 
-    public class BattleController : Battle
+    public class BattleController
     {
+
+        public int Turn { get; set; }
+        public int Turn_play { get; set; }
+        public string Character1_red { get; set; }
+        public string Character2_red { get; set; }
+        public string Character3_red { get; set; }
+
+
         private PlayerController playc;
         private IAController iac;
 
